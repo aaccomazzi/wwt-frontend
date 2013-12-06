8797ab7f4c2ad4c717012fa1c9cd7f4255f808b8
Chris Beaumont <beaumont@hawaii.edu>
2013-12-06 09:55:58 -0500

    smoothed heatmaps

--- toast/reproject.py
+++ toast/reproject.py
@@ -8,7 +8,7 @@ python reproject.py healpix_path_without_suffix
 from glob import glob
 from astropy.io import fits
 from healpy.projector import CartesianProj
-from healpy import vec2pix
+from healpy import vec2pix, smoothing, nest2ring, ring2nest
 import numpy as np
 from matplotlib.colors import LogNorm
 from skimage.io import imsave
@@ -17,7 +17,7 @@ from glue.clients.ds9norm import DS9Normalize
 NSIDE=512
 WIDTH=2600
 
-v2p = lambda x, y, z: vec2pix(NSIDE, x, y, z, nest=True)
+v2p = lambda x, y, z: vec2pix(NSIDE, x, y, z, nest=False)
 
 def make_map(m):
     p = CartesianProj(xsize=WIDTH)
@@ -36,8 +36,18 @@ def normalize(im):
     result = np.clip(result, 0, 255).astype(np.uint8)
     return result
 
+def smooth_map(im):
+    ind = np.arange(im.size)
+    ind = ring2nest(NSIDE, ind)
+    im = im[ind]
+    result =  smoothing(im, fwhm = np.radians(.3))
+    return result
+
+
 def build_image(m):
-    result = normalize(make_map(m))
+    result = smooth_map(m)
+    result = make_map(result)
+    result = normalize(result)
     result = np.flipud(result)
     return result
 
--- toast/toast.py
+++ toast/toast.py
@@ -8,10 +8,25 @@ from skimage.io import imread, imsave
 from glob import glob
 from lut import lut2pos
 import os
+import numpy as np
+from skimage.filter import median_filter
+
+def remove_spikes(im):
+    dtype = im.dtype
+    lo, hi = im.min(), im.max()
+
+    im2 = (1. * (im - lo) / (hi - lo))
+    fltr = median_filter(im2, radius=2)
+    diff = np.abs(1. * im2 - fltr)
+    fltr = (fltr * (hi - lo) + lo).astype(dtype)
+
+    bad = diff > max(np.median(diff) * 10, 5)
+    im[bad] = fltr[bad]
+    return im
 
 def resample(x, y, im):
-    x = lut2pos(x)
-    y = lut2pos(y)
+    x = remove_spikes(lut2pos(x))
+    y = remove_spikes(lut2pos(y))
     return im[y, x]
 
 
@@ -28,6 +43,7 @@ def toast(path):
             pth = os.path.join(base, file)
             x = imread(pth)
             y = imread(pth.replace('_x', '_y'))
+
             out = resample(x, y, im)
 
             outbase = base.replace('lut_x', folder)
